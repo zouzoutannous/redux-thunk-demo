7580e567b6eb52e4e2cc939c7515eb080788fb58
Joseph <zouzoutannousmakhoul@gmail.com>
2021-11-15 22:30:41 +0200

    Wiring up redux thunk

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import jsonplaceholder from "../apis/jsonPlaceholder";
+
+
+
+
+
+
+
+
+export const fetchPosts = () => async dispatch => {
+        const response = await jsonplaceholder.get('/posts');
+
+        dispatch({ type: 'FETCH_POSTS', payload: response})
+    };
